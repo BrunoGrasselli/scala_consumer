4c34e21e37dd63d96f0187cf89bf578c344176d1
David A. Eilertsen <david.eilertsen@sponsorpay.com>
2015-05-22 16:13:50 +0200

    Filter kafka messages

--- src/main/scala/consumer.scala
+++ src/main/scala/consumer.scala
@@ -7,40 +7,85 @@ import java.util.Properties
 import kafka.utils.Logging
 import scala.collection.JavaConversions._
 
+class Config(args: Array[String]) {
+  val config_string = args
+
+  def topic() : String = {
+    if(config_string.length > 0) config_string(0) else "tracking"
+  }
+
+  def host() : String = {
+    if(config_string.length > 1) config_string(1) else "localhost:2181"
+  }
+
+  def maxMessages() : Int = {
+    if(config_string.length > 2) config_string(2).toInt else 0
+  }
+
+  def filterKey() : Option[String] = {
+    if(config_string.length > 3) Some(config_string(3)) else None
+  }
+
+  def filterValue() : Option[String] = {
+    if(config_string.length > 4) Some(config_string(4)) else None
+  }
+
+  def groupId() : String = {
+    "consumer_123"
+  }
+
+  def isFiltering() : Boolean = {
+    !filterKey.isEmpty && !filterValue.isEmpty
+  }
+}
+
+
 object Consumer {
 
   def main(args: Array[String]): Unit = {
-    val topic = if(args.length > 0) args(0) else "tracking"
-    val zookeeper = if(args.length > 1) args(1) else "localhost:2181"
-    val max_messages = if(args.length > 2) args(2).toInt else 0
-    val group_id = "bruno_consumer_1234"
+    val config = new Config(args)
 
-    println(s"Topic: ${topic}, Zookeeper: ${zookeeper}")
+    println(s"Topic: ${config.topic}, Zookeeper host: ${config.host}")
 
     val props = new Properties()
 
-    ZkUtils.maybeDeletePath(zookeeper, s"/consumers/${group_id}");
+    ZkUtils.maybeDeletePath(config.host, s"/consumers/${config.groupId}");
 
-    props.put("group.id", group_id)
-    props.put("zookeeper.connect", zookeeper)
+    props.put("group.id", config.groupId)
+    props.put("zookeeper.connect", config.host)
     props.put("auto.offset.reset", "largest")
     props.put("auto.commit.enable", "false")
 
-    val config = new ConsumerConfig(props)
+    val consumer_config = new ConsumerConfig(props)
 
-    val connector = kafka.consumer.Consumer.create(config)
+    val connector = kafka.consumer.Consumer.create(consumer_config)
 
-    val filterSpec = new Whitelist(topic)
+    val filterSpec = new Whitelist(config.topic)
 
     val stream = connector.createMessageStreamsByFilter(filterSpec, 1, new DefaultDecoder(), new DefaultDecoder()).get(0)
 
     var message_count = 0
+    var message_filter_count = 0
+    var running = true
+
     for (messageAndMetadata <- stream) {
       message_count = message_count + 1
-      if (max_messages > 0 && message_count > max_messages) {
+
+      if (config.maxMessages > 0 && message_count > config.maxMessages) {
         connector.shutdown()
-      } else {
-        println(MessagePackSerialization.unpack(messageAndMetadata))
+      }
+      else {
+        val message = MessagePackSerialization.unpack(messageAndMetadata)
+
+        if (config.isFiltering)  {
+          if (message.get(config.filterKey.get).get == config.filterValue.get) {
+            message_filter_count = message_filter_count + 1
+            println(message + " | Message count -> " + {message_filter_count})
+          }
+        }
+        else {
+          println(message)
+        }
       }
     }
   }
